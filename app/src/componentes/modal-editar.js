ac083af7a0b904716f58b823fe7f2a188979e16b
LucasGoldman5 <lucasgoldman5@gmail.com>
2023-04-27 17:07:22 -0300

    optimizacion de modales y logica

--- app/src/componentes/modal-editar.js
+++ app/src/componentes/modal-editar.js
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import './modales.css'
 
 
-const ModalEditar = ({abrirModalEditar, itemToEdit, editar, cerrarFormulario,onsubmit, dataBrands}) => {
+const ModalEditar = ({abrirModalEditar, itemToEdit, editar, cerrarFormulario,onsubmit, dataBrands,errores}) => {
 
   const location = window.location.href
 const allDate = new Date()
@@ -16,7 +16,7 @@ const minutes = allDate.getMinutes()
 const date= `${Day}/${month}/${year}  Hora: ${hours}:${minutes}`;
 
 
-const { register,formState: { errors }, handleSubmit} = useForm ();
+const { register, handleSubmit} = useForm ();
 
 if(location ==="http://localhost:3000/Tabla/?txt=brands" ){
 
@@ -32,10 +32,11 @@ if(location ==="http://localhost:3000/Tabla/?txt=brands" ){
         <ModalBody>
           <form className="form-group" onSubmit={handleSubmit(onsubmit)}>
             <label htmlFor="id">ID</label>
-            <input className="form-control" type="number" name="id" id="id" readOnly  defaultValue={itemToEdit ? itemToEdit.id : ''}></input>
+            <input className="form-control" type="number" name="id" id="id" readOnly  defaultValue={itemToEdit ? itemToEdit.id : ''} {...register('id',{ shouldUnregister: true,})} />
             <br />
             <label htmlFor="marca">Marca</label>
-            <input className="form-control" type="text" name="marca" id="marca"  defaultValue={itemToEdit ? itemToEdit.marca : ''}{...register('title',{ shouldUnregister: true,})}/>
+            <input className="form-control" type="text" name="marca" id="marca"  defaultValue={itemToEdit ? itemToEdit.title : ''}{...register('title',{ shouldUnregister: true,})}/>
+            {errores.title? <p className="p-errores">El campo Marca debe ser definido</p> : ""}
             <br />
             <label htmlFor="url">Url</label>
             <input className="form-control" type="text" name="url" id="url"  defaultValue={itemToEdit ? itemToEdit.url : ''}{...register('url',{ shouldUnregister: true,})} />
@@ -70,19 +71,18 @@ if(location ==="http://localhost:3000/Tabla/?txt=brands" ){
             <br/>
             <label htmlFor="marca">Modelo</label>
             <input className="form-control" type="text" name="modelo" id="modelo"  defaultValue={itemToEdit ? itemToEdit.model : ''} {...register('model',{ shouldUnregister: true,})} />
+            {errores.model? <p className="p-errores">El campo Modelo debe ser definido</p> : ""}
             <br />
             <label htmlFor="url">Url</label>
             <input className="form-control" type="text" name="url" id="url"  defaultValue={itemToEdit ? itemToEdit.url : ''}{...register('url',{ shouldUnregister: true,})} />
             <br />
             <label htmlFor="url">Marca</label>
-            <select className="form-select" name="select" required {...register('brand_id',{
-                  required:true
-                })}>
+            <select className="form-select" name="select" required {...register('brand_id')}>
                   {dataBrands.map((brand)=>{
                     return <option key={brand.id} value={brand.id} >{brand.title}</option>
                   })}
                 </select>
-                {errors.brand_id?.type === 'required' && <p className="p-errores">Seleccione una marca</p>}
+                {errores.brand_id?<p className="p-errores">Haga click en una marca</p> : ""}
                 <br/>
             <div className="contenedor-boton-modal-dentro">
                 <button className="btn btn-success" type="submit" onClick={editar}>
@@ -110,11 +110,15 @@ if(location ==="http://localhost:3000/Tabla/?txt=brands" ){
         </ModalHeader>
         <ModalBody>
           <form className="form-group" onSubmit={handleSubmit(onsubmit)}>
+            <input className="form-control" type="text" readOnly name="id" defaultValue={itemToEdit ? itemToEdit.id : ""} {...register('id',{ shouldUnregister: true,})} />
+            <br/>
             <label htmlFor="descripcion">Descripcion</label>
             <input className="form-control" type="text" name="modelo" id="modelo"  defaultValue={itemToEdit ? itemToEdit.description : ''} {...register('description',{ shouldUnregister: true,})} />
+            {errores.description? <p className="p-errores">{errores.description}</p>: ""}
             <br />
             <label htmlFor="number">Numero de Telefono</label>
             <input className="form-control" type="text" name="number" id="number"  defaultValue={itemToEdit ? itemToEdit.phone_number : ''}{...register('phone_number',{ shouldUnregister: true,})} />
+            {errores.phone_number? <p className="p-errores">{errores.phone_number}</p> : ""}
             <br />
             <label htmlFor="direccion">Direccion</label>
             <input className="form-control" type="text" name="direccion" id="direccion"  defaultValue={itemToEdit ? itemToEdit.address : ''}{...register('address',{ shouldUnregister: true,})} />
