94ff8e71a702447e6edb95fc84f589c599e99d54
Martin Montenegro <martin.montenegro@xoor.io>
2024-04-12 11:14:06 -0300

    commit

---
+++ app/src/helpers/getEnviroment.js
@@ -0,0 +1,15 @@
+
+import env from '../enviroment.example.json';
+/* global getEnviroment */
+export default async function getEnviroment(){
+
+    /*const enviroment = await fetch('../enviroment.json')
+    .then(res  => res.json())
+    .then( datos =>{
+        return datos
+      }) 
+    .catch(error => {
+        return { message: `<p class="error"> Error al obtener el archivo enviroment.json: ${error}</p>`, error};
+      });*/
+    return env;
+}
